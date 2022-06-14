dc505f276ccfb2a2dbb262ecf3f76bbdbbf8d07c
hyjoong <maplesosser@naver.com>
2022-06-14 22:53:21 +0900

    fix: 경로 에러 해결

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import DashHeader from "./DashHeader";
-import TotalAd from "./TotalAd";
+import TotalAd from "./TotalAd/TotalAd";
 
 const Dashboard = () => {
   return (

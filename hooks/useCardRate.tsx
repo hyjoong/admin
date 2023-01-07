828b605c7a003f1fa1cfe687a408eff80f849afc
henry <70426440+hyjoong@users.noreply.github.com>
2023-01-07 19:59:27 +0900

    feat: 비교 데이터 없을시 null return

--- hooks/useCardRate.tsx
+++ hooks/useCardRate.tsx
@@ -27,46 +27,66 @@ export const useCardDiff = (totalData: IRateCard, prevTotalData: IRateCard) => {
     {
       title: "ROAS",
       value: `${totalData.roas.toFixed(2).toLocaleString()}%`,
-      calc: `${Math.abs(totalData.roas - prevTotalData.roas)
-        .toFixed(2)
-        .toLocaleString()}%`,
-      increase: totalData.roas - prevTotalData.roas > 0,
+      calc: isNaN(prevTotalData.cost)
+        ? ""
+        : `${Math.abs(totalData.roas - prevTotalData.roas)
+            .toFixed(2)
+            .toLocaleString()}%`,
+      increase: isNaN(prevTotalData.cost)
+        ? null
+        : totalData.roas - prevTotalData.roas > 0,
     },
     {
       title: "광고비",
       value: `${totalData.cost.toLocaleString()}원`,
-      calc: `${Math.abs(
-        totalData.cost - prevTotalData.cost
-      ).toLocaleString()}원`,
-      increase: totalData.cost - prevTotalData.cost > 0,
+      calc: isNaN(prevTotalData.cost)
+        ? ""
+        : `${Math.abs(totalData.cost - prevTotalData.cost).toLocaleString()}원`,
+      increase: isNaN(prevTotalData.cost)
+        ? null
+        : totalData.cost - prevTotalData.cost > 0,
     },
     {
       title: "클릭 수",
       value: `${totalData.click.toLocaleString()}회`,
-      calc: `${Math.abs(totalData.click - prevTotalData.click)}회`,
-      increase: totalData.click - prevTotalData.click > 0,
+      calc: isNaN(prevTotalData.click)
+        ? ""
+        : `${Math.abs(totalData.click - prevTotalData.click)}회`,
+      increase: isNaN(prevTotalData.click)
+        ? null
+        : totalData.click - prevTotalData.click > 0,
     },
     {
       title: "노출 수",
       value: `${totalData.imp.toLocaleString()}회`,
-      calc: `${Math.abs(totalData.imp - prevTotalData.imp).toLocaleString()}회`,
-      increase: totalData.imp - prevTotalData.imp > 0,
+      calc: isNaN(prevTotalData.imp)
+        ? ""
+        : `${Math.abs(totalData.imp - prevTotalData.imp).toLocaleString()}회`,
+      increase: isNaN(prevTotalData.imp)
+        ? null
+        : totalData.imp - prevTotalData.imp > 0,
     },
     {
       title: "전환 수",
       value: `${totalData.conv.toLocaleString()}회`,
-      calc: `${Math.abs(
-        totalData.conv - prevTotalData.conv
-      ).toLocaleString()}회`,
-      increase: totalData.conv - prevTotalData.conv > 0,
+      calc: isNaN(prevTotalData.cost)
+        ? ""
+        : `${Math.abs(totalData.conv - prevTotalData.conv).toLocaleString()}회`,
+      increase: isNaN(prevTotalData.conv)
+        ? null
+        : totalData.conv - prevTotalData.conv > 0,
     },
     {
       title: "매출",
       value: `${totalData.sales.toLocaleString()}원`,
-      calc: `${Math.abs(
-        totalData.sales - prevTotalData.sales
-      ).toLocaleString()}원`,
-      increase: totalData.sales - prevTotalData.sales > 0,
+      calc: isNaN(prevTotalData.sales)
+        ? ""
+        : `${Math.abs(
+            totalData.sales - prevTotalData.sales
+          ).toLocaleString()}원`,
+      increase: isNaN(prevTotalData.sales)
+        ? null
+        : totalData.sales - prevTotalData.sales > 0,
     },
   ];
 };

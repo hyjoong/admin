3686a50f3852e84612b2b300e409682672f57996
henry <70426440+hyjoong@users.noreply.github.com>
2023-01-15 17:46:36 +0900

    feat: stock api

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
+import https from "https";
+
+axios.defaults.httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
 export const instance = axios.create({
   baseURL:
--- api/stock/index.ts
+++ api/stock/index.ts
@@ -1,8 +1,24 @@
 import { instance } from "api";
-import { Stock } from "./type";
+import { Stock, StockDate } from "./type";
 
-export const getStock = async ({ numOfRows }: Stock) => {
+export const getStock = async ({ numOfRows, itmsNm }: Stock) => {
   return await instance.get(
-    `getStockPriceInfo?serviceKey=${process.env.NEXT_PUBLIC_STOCK_KEY}&numOfRows=${numOfRows}&resultType=json`
+    `getStockPriceInfo?serviceKey=${
+      process.env.NEXT_PUBLIC_STOCK_KEY
+    }&numOfRows=${numOfRows}&itmsNm=${encodeURI(itmsNm)}&resultType=json`
+  );
+};
+
+export const getStockDate = async ({
+  startDate,
+  endDate,
+  itmsNm,
+}: StockDate) => {
+  return await instance.get(
+    `getStockPriceInfo?serviceKey=${
+      process.env.NEXT_PUBLIC_STOCK_KEY
+    }&resultType=json&beginBasDt=${startDate}&endBasDt=${endDate}&itmsNm=${encodeURI(
+      itmsNm
+    )}`
   );
 };

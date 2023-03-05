6a3c79f6a655bc18a76b2cb06c40b6f4d4d8e402
henry <70426440+hyjoong@users.noreply.github.com>
2023-03-05 13:10:56 +0900

    feat: axios intance header cache setting

--- api/index.ts
+++ api/index.ts
@@ -6,4 +6,7 @@ axios.defaults.httpsAgent = new https.Agent({ rejectUnauthorized: false });
 export const instance = axios.create({
   baseURL:
     "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService",
+  headers: {
+    "Cache-Control": "public, max-age=300, stale-while-revalidate=86400",
+  },
 });
